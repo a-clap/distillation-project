925236e3f87fb65166c37ac34f53fe06cf95723a
a-clap <clapinskiadam@gmail.com>
2023-11-13 19:29:46 +0100

    quick fix: add arm64 opts

---
+++ embedded/cmd/embedded/opts_arm64.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"embedded/pkg/embedded"
+
+	"github.com/a-clap/logging"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+func setupLogging() {
+	logger := logging.GetLogger()
+
+	cfg := zap.NewProductionEncoderConfig()
+	rot := logging.RotateOptions{
+		MaxSize:    128,
+		MaxAge:     31,
+		MaxBackups: 2,
+		LocalTime:  false,
+		Compress:   true,
+	}
+	logger.AddRotateFileHandler(cfg, logging.DebugLevel, "/var/log/embedded/embedded.log", rot)
+
+	dsLogger := logging.GetLogger("ds18b20")
+	dsLogger.AddRotateFileHandler(cfg, logging.DebugLevel, "/var/log/embedded/ds18b20.log", rot)
+
+	maxLogger := logging.GetLogger("max31865")
+	maxLogger.AddRotateFileHandler(cfg, logging.DebugLevel, "/var/log/embedded/max31865.log", rot)
+
+	gpioLogger := logging.GetLogger("gpio")
+	gpioLogger.AddRotateFileHandler(cfg, logging.DebugLevel, "/var/log/embedded/gpio.log", rot)
+
+	heaterLogger := logging.GetLogger("heater")
+	heaterLogger.AddRotateFileHandler(cfg, logging.DebugLevel, "/var/log/embedded/heater.log", rot)
+}
+
+func getOpts(configPath string) ([]embedded.Option, []error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath(configPath)
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	cfg := embedded.Config{}
+	if err := viper.Unmarshal(&cfg); err != nil {
+		panic(err)
+	}
+
+	return embedded.Parse(cfg)
+}
